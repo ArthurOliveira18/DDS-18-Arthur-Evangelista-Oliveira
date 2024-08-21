39ad2b73e7f48560c37863a2fe27b47efdc0fa7d
Arthur <arthur.e.oliveira@aluno.senai.br>
2024-08-21 14:36:58 -0300

    Primeira aula de JS

--- Aula-9-JS/index.js
+++ Aula-9-JS/index.js
@@ -1,3 +1,16 @@
-alert("SADSADSADSADSADSADSADSADSAD")
+// alert("SADSADSADSADSADSADSADSADSAD")
 
 console.log("kdsaodksaokdosadk")
+
+
+var texto = "Kanye West"
+var numero = 1977
+var theBest = true
+var nulo = null
+var indefinido 
+
+console.log(texto)
+console.log(numero)
+console.log(theBest)
+console.log(nulo)
+console.log(indefinido)
