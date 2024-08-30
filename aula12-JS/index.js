8f12d12f9fda0a40fcbf7f662aff0bf66878014e
Arthur <arthur.e.oliveira@aluno.senai.br>
2024-08-30 16:17:00 -0300

    Mexendo com docmunt JS

--- aula12-JS/index.js
+++ aula12-JS/index.js
@@ -22,16 +22,42 @@ console.log(titulo)
 console.log(paragrafos)
 console.log(para3)
 
-paragrafos[0].innerText = "salada" //Mudando o conteudo do html
+// paragrafos[0].innerText = "salada" //Mudando o conteudo do html
 
-para3.innerHTML = "<h3>Substiui o id do p3 </h3>" //mudando o PROPRIO html
-
-
-p1.style.color = "green"
-p1.style.backgroundColor = "Blue"
-p1.style.color = "#fff"
+// para3.innerHTML = "<h3>Substiui o id do p3 </h3>" //mudando o PROPRIO html
 
 
+// p1.style.color = "green"
+// p1.style.backgroundColor = "Blue"
+// p1.style.color = "#fff"
+
+function cliquei(){
+    console.log("Cliquei e deu merda")
+}
+
+function outroFoco(){
+    console.log("Mudei o foco, não está mais na caixa")
+}
+
+// function textoPraTrocar(){
+//     console.log("Estou digitando")
+// }
+
+function trocaTexto(textoCaixinha){
+    let texto = document.getElementById("textoPra")
+    texto.innerText = textoCaixinha.value
+}
+
+function corVerde(elemento){
+    elemento.style.color = "green"
+    elemento.style.fontSize = "40px"
+}
+
+
+function corAzul(elemento){
+    elemento.style.color = "blue"
+    elemento.style.fontSize = "40px"
+}
 
 
 
